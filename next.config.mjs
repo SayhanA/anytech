3a3ce39b6c200f2e2af562b8216acab2fdf8198e
Sayhan Ahmed <sayhan@ewnbd.com>
2025-02-01 21:58:16 +0600

    Navbar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["cdn.sanity.io"], // Add the required domain
+  },
+};
 
 export default nextConfig;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section
+      id="home-hero__content__container"
+      className="lg:h-[max(620px,_calc(92vh-49px))] h-auto relative w-full text-white lg:bg-clip-hero-container overflow-hidden pt-[30%] md:pt-[20%] lg:pt-0 max-lg:pb-[15%] svelte-n9olsd"
+    ></section>
+  );
+};
+
+export default Hero;
